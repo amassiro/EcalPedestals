f1643cc0ff561896062871f76b2da16d081df8ee
amassiro <massironi.andrea@gmail.com>
2018-11-19 16:19:14 +0100

    new plotting tool

--- README.md
+++ README.md
@@ -12,7 +12,12 @@ Code to plot pedestals extracted from laser sequence
     
     
     r00t draw.cxx\(\"ana_ped_2016-2017.root\",0,283270\)
+
     
+    Showing cosmics and collisions
+    r00t drawWithType.cxx\(\"ana_ped_2016-2017.root\",0\)
+
+        
     r00t -q -b drawAll.cxx\(\"ana_ped_2016-2017.root\",283270\)
     
     
@@ -53,4 +58,48 @@ ROOT Tree content
     chan[] is the channel number 
     ped[] is the pedestal 
     pedrms[] is the pedestal rms 
+
+
+    
+    
+    
+    
+g.SetPoint(1-1,1,132.46/132.46)
+g.SetPoint(2-1,2,134.67/132.46)
+g.SetPoint(3-1,3,136.62/132.46)
+g.SetPoint(4-1,4,136.86/132.46)
+g.SetPoint(5-1,5,136.22/132.46)
+g.SetPoint(6-1,6,134.71/132.46)
+g.SetPoint(7-1,7,133.22/132.46)
+g.SetPoint(8-1,8,131.19/132.46)
+
+
+
+
+ee->Draw("eflow_abs:avg_time >> gr","ix==70 && iy==30 && iring>0");
+
+
+
+ee->Draw("eflow_abs:avg_time >> gr","ix==55 && iy==35 && iring>0");
+ee->Draw("eflow_abs:avg_time >> gr(100000,1462060800,1475650000,1000,0.1,2.5)","ix==56 && iy==36 && iring<0 && n_events>8e6");
+
+ee->Draw("eflow_abs:avg_time >> gr(100000,1462060800,1475650000,1000,0.5,1.5)","ix==56 && iy==35 && iring<0 && n_events>8e6");
+
+ee->Draw("eflow_abs:avg_time >> gr(100000,1462060800,1475650000,1000,0.5,1.5)","ix==56 && iy==36 && iring<0 && n_events>8e6");
+
+
+ee->Draw("eflow_abs:avg_time >> gr(1000,1495060800,1511650000,1000,0.5,1.5)","ix==56 && iy==36 && iring<0 && n_events>8e3");
+
+ee->Draw("eflow_abs:avg_time","ix==56 && iy==36 && iring<0");
+
+ 
+gr->Draw("PL");
+gr->SetMarkerSize(2);
+gr->SetMarkerStyle(20);
+gr->GetXaxis()->SetTimeDisplay(1);
+gr->GetXaxis()->SetTimeFormat("%d/%m%F1970-01-01 00:00:00");
+
+gr->Draw("PL");
+
+
 
---
+++ drawWithType.cxx
@@ -0,0 +1,187 @@
+//
+//---- plot pedestals
+//
+
+void drawWithType(std::string nameInputFile = "ana_ped_2016-2017.root", int index = 0, int runNumber = -1) {
+  
+  std::cout << " runNumber = " << runNumber << std::endl;
+  
+  gStyle->SetOptStat(0);
+    
+  TFile* fileIn = new TFile(nameInputFile.c_str(), "READ");
+  TTree* T       = (TTree*) fileIn -> Get ("T");
+  TTree* PedChan = (TTree*) fileIn -> Get ("PedChan");
+  
+  Int_t           Channels;
+  Int_t           x;
+  Int_t           y;
+  Int_t           z;
+  
+  PedChan -> SetBranchAddress("Channels", &Channels);
+  PedChan -> SetBranchAddress("x", &x);
+  PedChan -> SetBranchAddress("y", &y);
+  PedChan -> SetBranchAddress("z", &z);
+  
+  PedChan->GetEntry(index);
+  
+  std::cout << " x :  " << x << std::endl;
+  std::cout << " y :  " << y << std::endl;
+  std::cout << " z :  " << z << std::endl;
+  
+  
+  Int_t           fed[75848];
+  
+  T -> SetBranchAddress ("fed", fed);
+  
+  
+  int nEntries = T->GetEntries();
+
+  std::cout << " nEntries = " << nEntries << std::endl;
+  
+//   
+//   run_type --> 1=collisions, 2=cosmics, 3=circulating, 4=test, 0=other not classified 
+//   
+  
+  //----> cosmics
+  
+  TEntryList* myListCosmics;
+  if (runNumber != -1) {
+    TString cut = Form ("run == %d && run_type==2", runNumber);
+    T -> SetEntryList(0); 
+    T -> Draw(">> myListCosmics",cut.Data(),"entrylist");
+    myListCosmics = (TEntryList*)gDirectory->Get("myListCosmics");
+    T -> SetEntryList(myListCosmics);    
+  }
+  else {
+    TString cut = Form ("run_type==2");
+    T -> SetEntryList(0); 
+    T -> Draw(">> myListCosmics",cut.Data(),"entrylist");
+    myListCosmics = (TEntryList*)gDirectory->Get("myListCosmics");
+    T -> SetEntryList(myListCosmics);        
+  }
+  
+  nEntries = T->GetEntries();
+  
+  std::cout << " --> nEntries cosmics = " << nEntries << std::endl;
+     
+  int fedNumber = -1;
+  
+  //---- decide which crystal and fed we will display
+  T->GetEntry(0);
+  fedNumber = fed [index] - 601; //---- first fed for ECAL is 601
+  
+  
+  TString ped_string  = Form ("ped[%d]", index);
+  TString rms_string  = Form ("pedrms[%d]", index);
+  TString time_string = Form ("time[%d]", fedNumber);
+  TString toDraw = Form ("%s : %s : %s", ped_string.Data(), rms_string.Data(), time_string.Data());
+  
+  T->Draw(toDraw.Data(), "", "goff");
+  TGraph *gr_ped_cosmics  = new TGraph(T->GetSelectedRows(), T->GetV3(), T->GetV1());  
+  TGraph *gr_rms_cosmics  = new TGraph(T->GetSelectedRows(), T->GetV3(), T->GetV2());  
+  
+  std::cout << "     --> nEntries cosmics T->GetSelectedRows() = " << T->GetSelectedRows() << std::endl;
+  
+  
+  //----> collisions
+  
+  
+  TEntryList* myListCollisions;
+  if (runNumber != -1) {
+    TString cut = Form ("run == %d && run_type==1", runNumber);
+    T -> SetEntryList(0); 
+    T -> Draw(">> myListCollisions",cut.Data(),"entrylist");
+    myListCollisions = (TEntryList*)gDirectory->Get("myListCollisions");
+    T -> SetEntryList(myListCollisions);    
+  }
+  else {
+    TString cut = Form ("run_type==1");
+    T -> SetEntryList(0); 
+    T -> Draw(">> myListCollisions",cut.Data(),"entrylist");
+    myListCollisions = (TEntryList*)gDirectory->Get("myListCollisions");
+    T -> SetEntryList(myListCollisions);        
+  }
+  
+  
+  
+  std::cout << " --> nEntries collisions = " << nEntries << std::endl;
+  
+  fedNumber = -1;
+  
+  //---- decide which crystal and fed we will display
+  T->GetEntry(0);
+  fedNumber = fed [index] - 601; //---- first fed for ECAL is 601
+  
+  
+  T->Draw(toDraw.Data(), "", "goff");
+  TGraph *gr_ped_collisions  = new TGraph(T->GetSelectedRows(), T->GetV3(), T->GetV1());  
+  TGraph *gr_rms_collisions  = new TGraph(T->GetSelectedRows(), T->GetV3(), T->GetV2());  
+
+  std::cout << "     --> nEntries collisions T->GetSelectedRows() = " << T->GetSelectedRows() << std::endl;
+  
+  
+  
+  
+  //---- style ----
+  
+  gr_ped_cosmics->SetMarkerSize  (1);               
+  gr_ped_cosmics->SetMarkerStyle (24);              
+  gr_ped_cosmics->SetMarkerColor (kRed);            
+  gr_ped_cosmics->SetLineWidth (1);                 
+  gr_ped_cosmics->SetLineColor (kRed);              
+  
+  gr_rms_cosmics->SetMarkerSize  (1);               
+  gr_rms_cosmics->SetMarkerStyle (24);              
+  gr_rms_cosmics->SetMarkerColor (kBlue);            
+  gr_rms_cosmics->SetLineWidth (1);                 
+  gr_rms_cosmics->SetLineColor (kBlue);              
+ 
+  
+  gr_ped_collisions->SetMarkerSize  (1);               
+  gr_ped_collisions->SetMarkerStyle (23);              
+  gr_ped_collisions->SetMarkerColor (kGreen);            
+  gr_ped_collisions->SetLineWidth (1);                 
+  gr_ped_collisions->SetLineColor (kGreen);              
+  
+  gr_rms_collisions->SetMarkerSize  (1);               
+  gr_rms_collisions->SetMarkerStyle (23);              
+  gr_rms_collisions->SetMarkerColor (kGreen);            
+  gr_rms_collisions->SetLineWidth (1);                 
+  gr_rms_collisions->SetLineColor (kGreen);              
+  
+  //---- style (end) ----
+  
+  
+  TCanvas* ccPed = new TCanvas ("ccPed","",1600,600);
+  gr_ped_cosmics->Draw("AP");
+  gr_ped_cosmics->GetYaxis()->SetTitle("ped ADC");
+  gr_ped_cosmics->GetXaxis()->SetTitle("time");
+  gr_ped_cosmics->GetXaxis()->SetTimeDisplay(1);
+  gr_ped_cosmics->GetXaxis()->SetNdivisions(-503);
+  gr_ped_cosmics->GetXaxis()->SetTimeFormat("%Y-%m-%d %H:%M");
+  gr_ped_cosmics->GetXaxis()->SetTimeOffset(0,"gmt");
+
+  gr_ped_collisions->Draw("P");
+  ccPed->SetGrid();
+  
+  
+  
+  TCanvas* ccRms = new TCanvas ("ccRms","",1600,600);
+  gr_rms_cosmics->Draw("AP");
+  gr_rms_cosmics->GetYaxis()->SetTitle("rms ADC");
+  gr_rms_cosmics->GetXaxis()->SetTitle("time");
+  gr_rms_cosmics->GetXaxis()->SetTimeDisplay(1);
+  gr_rms_cosmics->GetXaxis()->SetNdivisions(-503);
+  gr_rms_cosmics->GetXaxis()->SetTimeFormat("%Y-%m-%d %H:%M");
+  gr_rms_cosmics->GetXaxis()->SetTimeOffset(0,"gmt");
+ 
+  gr_rms_collisions->Draw("P");
+  
+  ccRms->SetGrid();
+  
+  
+}
+
+
+
+
